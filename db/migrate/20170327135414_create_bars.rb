b3218c490cda7366030f6a93940e9e6618eb8bdf
Ollie Warren <oliverwarren1@hotmail.com>
2017-03-27 15:26:35 +0100

    Setup devise

---
+++ db/migrate/20170327135414_create_bars.rb
@@ -0,0 +1,10 @@
+class CreateBars < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bars do |t|
+      t.string :name
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
