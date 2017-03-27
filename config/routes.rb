4600144a1f5df78793f92caca714d7940e470c78
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2017-03-27 18:10:05 +0200

    routing part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bars, only: [:show, :index, :new, :create] do
+    resources :events, only: [:new, :create, :destroy]
+  end
+  resources :events, only: [:index, :show]
 end
